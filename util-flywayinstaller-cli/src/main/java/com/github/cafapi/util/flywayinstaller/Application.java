57287623877c2bdcc1d3452bd72930c977fbd8c5
Xavier Lamourec <xavier.lamourec@microfocus.com>
2021-11-25 16:32:11 +0000

    US353257: Initial implementation (#1)

---
+++ util-flywayinstaller-cli/src/main/java/com/github/cafapi/util/flywayinstaller/Application.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2022 Micro Focus or one of its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cafapi.util.flywayinstaller;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import java.sql.SQLException;
+import java.util.concurrent.Callable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+
+@CommandLine.Command(
+    name = "util-flywayinstaller",
+    mixinStandardHelpOptions = true,
+    exitCodeListHeading = "%nExit Codes:%n",
+    exitCodeList = {" 0:Successful program execution.",
+                    " 1:Execution exception: An exception occurred while executing the migrations."
+    })
+public final class Application implements Callable<Integer>
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    private Application()
+    {
+    }
+
+    @CommandLine.Option(
+        names = {"-db.host"},
+        paramLabel = "<dbHost>",
+        required = true,
+        description = "Specifies the database host name. "
+        + "e.g. localhost"
+    )
+    private String dbHost;
+
+    @CommandLine.Option(
+        names = {"-db.port"},
+        paramLabel = "<dbPort>",
+        required = true,
+        description = "Specifies the database port to be used. "
+        + "e.g. 5432"
+    )
+    private int dbPort;
+
+    @CommandLine.Option(
+        names = {"-db.user"},
+        paramLabel = "<username>",
+        required = true,
+        description = "Specifies the username to access the database."
+    )
+    private String username;
+
+    @CommandLine.Option(
+        names = {"-db.pass"},
+        paramLabel = "<password>",
+        required = true,
+        description = "Specifies the password to access the database."
+    )
+    private String password;
+
+    @CommandLine.Option(
+        names = {"-db.name"},
+        paramLabel = "<dbName>",
+        required = true,
+        description = "Specifies the name of the database to be created or updated."
+    )
+    private String dbName;
+
+    @CommandLine.Option(
+        names = {"-log"},
+        paramLabel = "<logLevel>",
+        description = "Specifies the logging level of the installer. Can be DEBUG, INFO, WARNING, ERROR or OFF. Must be Uppercase"
+    )
+    private LogLevel logLevel;
+
+    public static void main(final String[] args)
+    {
+        final int exitCode = new CommandLine(new Application()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call()
+    {
+        if (logLevel != null) {
+            setLogLevel(logLevel);
+        }
+
+        try {
+            Migrator.migrate(dbHost, dbPort, dbName, username, password);
+        } catch (final SQLException | RuntimeException ex) {
+            LOGGER.error("Issue while migrating.", ex);
+            return 1;
+        }
+        return 0;
+    }
+
+    private static void setLogLevel(final LogLevel logLevel)
+    {
+        final LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.getLoggerList().forEach(tmpLogger -> tmpLogger.setLevel(Level.toLevel(logLevel.name())));
+        LOGGER.debug("Log level set to {}.", logLevel);
+    }
+}
---
+++ util-flywayinstaller/src/main/java/com/github/cafapi/util/flywayinstaller/Migrator.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2022 Micro Focus or one of its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cafapi.util.flywayinstaller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.flywaydb.core.Flyway;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Migrator
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Migrator.class);
+    private static final String CREATE_DATABASE = "SELECT format('CREATE DATABASE %I', ?)";
+    private static final String DOES_DATABASE_EXIST
+        = "SELECT EXISTS (SELECT NULL FROM pg_catalog.pg_database WHERE lower( datname ) = lower( ? ));";
+
+    private Migrator()
+    {
+    }
+
+    public static void migrate(
+        final String dbHost,
+        final int dbPort,
+        final String dbName,
+        final String username,
+        final String password
+    ) throws SQLException
+    {
+        logReceivedArgumentsIfDebug(dbHost, dbPort, dbName, username, password);
+
+        LOGGER.info("Checking connection ...");
+
+        final PGSimpleDataSource dbSource = new PGSimpleDataSource();
+        dbSource.setServerNames(new String[]{dbHost});
+        dbSource.setPortNumbers(new int[]{dbPort});
+        dbSource.setUser(username);
+        dbSource.setPassword(password);
+
+        try (final Connection connection = dbSource.getConnection()) {
+            if (!doesDbExist(connection, dbName)) {
+                LOGGER.debug("reset or createDB");
+                createDatabase(connection, dbName);
+            }
+        }
+        LOGGER.info("Connection Ok. Starting migration ...");
+
+        // Once we made sure that the database exists, we add the information in the dataSource
+        dbSource.setDatabaseName(dbName);
+
+        final Flyway flyway = Flyway.configure()
+            .dataSource(dbSource)
+            .baselineOnMigrate(true)
+            .load();
+        flyway.migrate();
+        LOGGER.info("DB update finished.");
+    }
+
+    private static void createDatabase(
+        final Connection connection,
+        final String dbName
+    ) throws SQLException
+    {
+        final String createDbQuery = getCreateDbQuery(connection, dbName);
+
+        try (final Statement createDbStatement = connection.createStatement()) {
+            createDbStatement.executeUpdate(createDbQuery);
+            LOGGER.info("Created new database: {}", dbName);
+        }
+    }
+
+    private static String getCreateDbQuery(final Connection connection, final String dbName) throws SQLException
+    {
+        try (final PreparedStatement getCreateDbQueryStatement = connection.prepareStatement(CREATE_DATABASE)) {
+            getCreateDbQueryStatement.setString(1, dbName);
+            final ResultSet set = getCreateDbQueryStatement.executeQuery();
+            set.next();
+            return set.getString(1);
+        }
+    }
+
+    private static boolean doesDbExist(
+        final Connection connection,
+        final String dbName
+    ) throws SQLException
+    {
+        try (final PreparedStatement statement = connection.prepareStatement(DOES_DATABASE_EXIST)) {
+            statement.setString(1, dbName);
+            final ResultSet set = statement.executeQuery();
+            set.next();
+            return set.getBoolean(1);
+        }
+    }
+
+    private static void logReceivedArgumentsIfDebug(
+        final String dbHost,
+        final int dbPort,
+        final String dbName,
+        final String username,
+        final String password
+    )
+    {
+        LOGGER.debug("Arguments received"
+            + " dbHost: {}"
+            + " dbPort: {}"
+            + " dbName: {}"
+            + " username: {}"
+            + " password: {}", dbHost, dbPort, dbName, username, password);
+    }
+}
