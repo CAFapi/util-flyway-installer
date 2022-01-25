58f7d852e74a40e83c3d96f3cafd05065bf205e2
Xbreizh <xavier.lamourec@gmail.com>
2022-01-25 21:51:31 +0000

    fix

--- util-flywayinstaller/src/main/java/com/github/cafapi/util/flywayinstaller/Migrator.java
+++ util-flywayinstaller/src/main/java/com/github/cafapi/util/flywayinstaller/Migrator.java
@@ -69,7 +69,7 @@ public final class Migrator
                 .baselineOnMigrate(true)
                 .load();
         flyway.migrate();
-        LOGGER.info("Migration completed ...");
+        LOGGER.info("DB update finished.");
     }
 
     private static void createDatabase(
@@ -79,7 +79,7 @@ public final class Migrator
     {
         try (final PreparedStatement statement = connection.prepareStatement(String.format(CREATE_DATABASE, dbName))){
             statement.executeUpdate();
-            LOGGER.info("DB update finished.");
+            LOGGER.info("Created new database: {}", dbName);
         }
     }
 
